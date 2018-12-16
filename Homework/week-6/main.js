9db4d461b962a1ddee4874fa3e7496612d5b7927
CUniversityaccount <44020631+CUniversityaccount@users.noreply.github.com>
2018-12-16 02:48:51 +0100

    af

--- Homework/week-6/main.js
+++ Homework/week-6/main.js
@@ -4,73 +4,38 @@
 
 
 function parseData(data) {
-  var totalData = [];
-  var female = [];
-  var male = []
-  var parseYear = d3.timeParse("%Y");
-  var formatYear = d3.timeFormat("%Y");
-  let dataKeys = Object.keys(data)
-
-  data.forEach(function(data) {
-      if (data.SEX === "MA") {
-        male.push({total: data.Value, country: data.Country, year: data.TIME});;
-      }
-      else if (data.SEX === "FE") {
-        female.push({total: data.Value, country: data.Country, year: data.TIME});
-      }
-      else if (data.SEX === "TT") {
-        totalData.push({"country": data.Country, "year": data.TIME, "total": data.Value});
-      }
-    });
-
-  for (var c = 0; c < totalData.length; c++) {
-    for (var i = 0; i < female.length; i++ ) {
-      if (totalData[c].year === female[i].year && totalData[c].country === female[i].country) {
-        totalData[c]["female"] = female[i].total
-      }
-      else if (totalData[c].year === male[i].year && totalData[c].country === male[i].country) {
-        totalData[c]["male"] = male[i].total
-      };
-    };
-  };
-  return totalData
+  var groupData = d3.nest()
+                    .key(function(d) { return d.Time})
+                    .key(function(d) { return d.Country})
+                    .key(function(d) {  var split = d.Subject.split(" ")
+                                        return split[split.length - 1]})
+                    .rollup(function(d) {
+                            var dict = {}
+                            d.forEach( function(dp) {
+                              dict[dp.Sex] = parseInt(dp.Value)
+                              dict["country"] = dp.Country
+                            })
+                            return dict
+                          })
+                    .object(data);
+  return groupData
 };
 
 function parseCountry(data) {
-  var dataArray = {country: [], year: []}
-
-  data.forEach( function(dp) {
-    Object.keys(dataArray).forEach (function(key) {
-      if (dataArray[key].length === 0) {
-        dataArray[key].push(dp[key])
-      }
-      else if (dataArray[key].includes(dp[key])){
-      }
-      else {
-        dataArray[key].push(dp[key])
-      };
-    })
-  });
-
-  return dataArray
-}
-
-function dataSelection(data, year) {
   var dataArray = []
-
-  data.forEach( function(dp) {
-    if (dp.year === year) {
-      dataArray.push(dp)
-    }
-  });
+  Object.keys(data).forEach(function (d) {
+      dataArray.push(d)
+  })
 
   return dataArray
 }
+
 
 function makeGraph(data) {
-  let countryYears = parseCountry(data);
+  let years = Object.keys(data);
 
-  var beginDate = countryYears.year[0];
+  var beginDate = years[0]
+  let country = parseCountry(data[beginDate])
 
   // dropdown menu
   const dropdown = d3.select("body")
@@ -79,126 +44,219 @@ function makeGraph(data) {
   const menu = d3.select("#dropdown")
                   .append("select")
                   .selectAll("option")
-                    .data(countryYears.year).enter()
+                    .data(years).enter()
                     .append('option')
                     .text( function (d) { return d})
                     .attr( "value", function (d) {
                                     return d; })
 
-  var selectedData = dataSelection(data, beginDate)
-  makeBar(data, selectedData, countryYears)
+  makeBar(data, data[beginDate], country, years)
 }
 
-function makeBar(data, selectedData, year) {
+function makeBar(data, selectedData, country, year) {
+  const stack = d3.stack().offset(d3.stackOffsetExpand);
   const margin = 80;
   const width = 750 - 2 * margin;
   const height = 500 - 2 * margin;
+  var list = Object.keys(selectedData[country[0]]).sort(function(d) { return d } )
+  var index = list.indexOf("ages")
+  list.splice(index, 1)
+  list.sort()
+
+  console.log(list)
+  var z = d3.scaleOrdinal(d3.schemeCategory10)
+            .domain(list);
+
 
   const svg = d3.select("body")
               .append("svg")
               .attr("class", "bargraph")
-              .attr("width", (width+ 2 * margin) + "px")
+              .attr("width", (width+ 2 * margin + 50) + "px")
               .attr("height", height + 2 * margin);
+              g = svg.append("g").attr("transform", "translate(" + margin + "," + margin + ")")
+
   // x_axis
   let xAxis = d3.scaleBand()
                   .range([0, width])
                   .padding(0.1)
-                  .domain(year.country)
+                  .domain(country)
+
   // y_axis
-  let yValue = function (d) { return d.total};
+  function yValue(data) {
+    let max = []
+    Object.values(data).forEach(function (d) {
+      Object.values(d).forEach(function (dp) {
+      max.push(dp["All persons"])
+    })
+    });
+    return d3.max(max)
+  }
+  var max = yValue(selectedData)
   let yAxis = d3.scaleLinear()
                 .range([height, 0])
-                .domain([0, d3.max(data, yValue)])
+                .domain([0, max])
 
 
+  var dataList = []
 
-  // Makes the bars
-  d3.select("svg.bargraph")
-    .selectAll(".bar")
-    .data(selectedData)
-    .enter().append("rect")
+  Object.values(selectedData).forEach(function(d) {
+      var dict = {}
+    list.forEach(function (dp) {
+      console.log(d[dp].country)
+      dict["country"] = d[dp].country
+      dict[dp] = d[dp]["All persons"]
+    });
+      dataList.push(dict)
+  })
+
+  var stackData = stack.keys(list)(dataList)
+  var serie = g.selectAll(".series")
+              .data(stackData)
+              .enter()
+              .append("g")
+                .attr("class", "series")
+                .attr('fill', function(d) { return z(d.key) })
+                .attr("key", function (d) { return d.key});
+
+    serie.selectAll(".series")
+      .data(function(d) { return d})
+      .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return xAxis(d.country); })
-      .attr("y", function(d) { return yAxis(d.total); })
+      .attr("x", function(d) { return xAxis(d.data.country); })
+      .attr("y", function(d) { return yAxis(d[1] * selectedData[d.data.country]["ages"]["All persons"]); })
       .attr("width", xAxis.bandwidth())
-      .attr("height", function(d) { return height - yAxis(d.total); })
-      .attr("transform", "translate(" + margin + "," + margin + ")")
-      .on("click", function (d) {
-        d3.selectAll(this)
-        if (this.style.fill === "grey") {
-          d3.selectAll("rect").style("fill", "grey")
-          d3.select(this).style("fill", "red")
-          makePie(d)
+      .attr("height", function(d) { return yAxis(d[0] *selectedData[d.data.country]["ages"]["All persons"]) - yAxis(d[1] * selectedData[d.data.country]["ages"]["All persons"]); })
+      .attr("selected", "no")
+      .on("click", function(d) {
+        console.log(d)
+        if (d3.select(this).attr("selected") === "yes"){
+          d3.selectAll(".bar").attr("selected", "no")
+          d3.select(this).attr("stroke", "none")
+          d3.select("svg.piechart").remove()
         }
         else {
-          d3.select("svg.piechart").remove();
-          d3.select(this).style("fill", "grey")
-
+          d3.selectAll(".bar").attr("selected", "no").attr("stroke", "none")
+          d3.select(this).attr("selected", "yes")
+          d3.select(this).attr("stroke", "black")
+          d3.select(this).attr("stroke-width", "2px")
+          var key = d3.select(this.parentNode).attr("key")
+          makePie(selectedData[d.data.country][key])
         }
       });
 
-  // call x_axis
-  svg.append("g")
-      .attr("id", "x_axis")
-      .attr("class", "axis")
-      .attr("transform", "translate(" + margin + "," + (+ margin + height) + ")")
-      .call(d3.axisBottom(xAxis))
+    // call x_axis
+   svg.append("g")
+        .attr("id", "x_axis")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + margin + "," + (+ margin + height) + ")")
+        .call(d3.axisBottom(xAxis))
 
-  // call y_axis
-  svg.append("g")
-      .attr("id", "y_axis")
-      .attr("class", "axis")
-      .attr("transform", "translate(" + margin + "," + margin + ")")
-      .call(d3.axisLeft(yAxis))
+    //  call y_axis
+    svg.append("g")
+        .attr("id", "y_axis")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + margin + "," + margin + ")")
+        .call(d3.axisLeft(yAxis))
+
+    // legend
+    var legendDomain = z
+     var legend = svg.selectAll(".legend")
+         .data(list)
+       .enter().append("g")
+         .attr("class", "legend")
+         .attr("transform", function(d, i) { return "translate(" + 200 + "," + (height - 3 *  margin + i * 20) + ")"; })
+
+     // draw legend colored rectangles
+      legend.append("rect")
+          .attr("x", width - 18)
+          .attr("width", 10)
+          .attr("height", 15)
+          .style("fill", function(d) { return z(d)})
+          .style("stroke", "black");
+
+      // draw legend text
+      legend.append("text")
+          .attr("x", width - 24)
+          .attr("y", 9)
+          .attr("dy", ".25em")
+          .style("text-anchor", "end")
+          .style("font-size", "12px")
+          .text(function(d) { return d;})
+
+
+
 
   d3.select("#dropdown").on('change', function () {
-      var newData = d3.select(this)
+      d3.select("svg.piechart").remove()
+      var newDate = d3.select(this)
                            .select("select")
                            .property("value");
-      var selectedData = dataSelection(data, newData)
-      updateGraph(data, selectedData, year, xAxis, yAxis);
+      updateGraph(data[newDate]);
 
+
+      function updateGraph(data) {
+        // update max value
+        function yValue(data) {
+          let max = []
+          Object.values(data).forEach(function (d) {
+            Object.values(d).forEach(function (dp) {
+            max.push(dp["All persons"])
+          })
+          });
+          return d3.max(max)
+        }
+        var max = yValue(selectedData)
+
+        var dataList = []
+
+        Object.values(data).forEach(function(d) {
+            var dict = {}
+          list.forEach(function (dp) {
+            console.log(d[dp].country)
+            dict["country"] = d[dp].country
+            dict[dp] = d[dp]["All persons"]
+          });
+            dataList.push(dict)
+        })
+
+        var stackData = stack.keys(list)(dataList)
+      g.selectAll(".series")
+                  .data(stackData)
+                  .enter()
+
+      d3.selectAll("rect").remove()
+
+      // change graph
+      serie.selectAll(".series")
+        .data(function(d) { return d})
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return xAxis(d.data.country); })
+        .attr("y", function(d) { return yAxis(d[1] * selectedData[d.data.country]["ages"]["All persons"]); })
+        .attr("width", xAxis.bandwidth())
+        .attr("height", function(d) { return yAxis(d[0] *selectedData[d.data.country]["ages"]["All persons"]) - yAxis(d[1] * selectedData[d.data.country]["ages"]["All persons"]); })
+        .attr("selected", "no")
+        .on("click", function(d) {
+          if (d3.select(this).attr("selected") === "yes"){
+            d3.selectAll(".bar").attr("selected", "no")
+            d3.select("svg.piechart").remove()
+          }
+          else {
+            d3.selectAll(".bar").attr("selected", "no")
+            d3.select(this).attr("selected", "yes")
+            var key = d3.select(this.parentNode).attr("key")
+            makePie(selectedData[d.data.country][key])
+          }
+        });
+
+      }
     })
 }
 
-function updateGraph(data, selectedData, year, xAxis, yAxis) {
-  const margin = 80;
-  const width = 750 - 2 * margin;
-  const height = 500 - 2 * margin;
-  d3.select("svg.bargraph").selectAll(".bar")
-    .remove()
-    .enter()
-    
-  d3.select("svg")
-    .selectAll(".bar")
-    .data(selectedData)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return xAxis(d.country); })
-      .attr("y", function(d) { return yAxis(d.total); })
-      .attr("width", xAxis.bandwidth())
-      .attr("height", function(d) { return height - yAxis(d.total); })
-      .attr("transform", "translate(" + margin + "," + margin + ")")
-      .on("click", function (d) {
-        d3.selectAll(this)
-        if (this.style.fill === "grey") {
-          d3.selectAll("rect").style("fill", "grey")
-          d3.select(this).style("fill", "red")
-          console.log(d)
-          makePie(d)
-        }
-        else {
-          d3.select("svg.piechart").remove();
-          d3.select(this).style("fill", "grey")
-
-        }
-      })
-}
-
 function makePie(data) {
-
+  console.log(data)
   var values = parsePie(data)
-  var color = d3.scaleOrdinal().range(["#FFC0CB", "#00008b"]);
+  var color = d3.scaleOrdinal().range(["#00008b", "#FFC0CB"]);
   var width = 960,
       height = 450,
       radius = Math.min(width, height) / 2;
@@ -208,7 +266,7 @@ function makePie(data) {
               .outerRadius(radius);
 
   var pie = d3.pie()
-              .value(function(d) { return (d.value / data.total); })
+              .value(function(d) { return (d / data["All persons"]); })
               .sort(null);
 
   var elementExists = document.getElementsByClassName("piechart")
@@ -221,7 +279,6 @@ function makePie(data) {
 
 
   }
-
   var svg = d3.select("svg.piechart")
               .append("g")
               .attr("transform", "translate("+ radius + ", " + radius + ")");
@@ -234,33 +291,25 @@ function makePie(data) {
               .attr('fill', function(d, i) {
                 return color(i);
               })
-              .text("text", function(d) { return d.label; });
 
   if (elementExists != 0) {
     var values = parsePie(data)
-    pie.value(function(d) { return (d.value / data.total); })
-    path = path.data(pie)
+    pie.value(function(d) { return (d / data["All persons"]); })
+    path = path.data(pie(values))
   }
 }
 
 function parsePie(data) {
-  var male = null
-  var female = null
-  Object.keys(data).forEach(function(dp) {
-    if (dp === "male") {
-      male = {label: dp, value: data[dp]}
-    }
-    else if (dp === "female") {
-      female = {label: dp, value: data[dp]}
-    }
-  })
+  console.log(data)
+  var male = data.Males
+  var female = data.Females
   return [male, female]
 }
 
 
 
 function main() {
-  d3.csv("data.csv").then(function (data) {
+  d3.csv("data3.csv").then(function (data) {
     var parsedData = parseData(data)
     makeGraph(parsedData)
   });
